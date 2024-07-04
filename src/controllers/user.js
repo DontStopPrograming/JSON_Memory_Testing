3d539f7ade8eae5657241d316ba52cfd9bc7b6fa
DontStopPrograming <01dyamond01@gmail.com>
2024-07-04 16:12:04 -0500

    Feat: completing CRUD REST API v1

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,13 +1,30 @@
 import user from '../data/user.json' assert { type: "json"}
 
-export const getUserList = (req, res) => {
+export const getsUserList = async (req, res) => {
     try {
-
-        res.send(user)
+        const users = await user
+        res.send(users)
     } catch (error) {
         console.error(error)
-        res.status(500).json({ error: 'Error in the list of users' })
+        res.status(500).json({ error: 'ERROR GET DATA' })
     }
 }
 
-export default getUserList
+export const getUserList = async (req, res) => {
+    try {
+        const users = await user
+        const userId = req.params.id
+        const findUser = users.find(u => u.id === userId)
+        if (findUser) {
+            res.send(findUser)
+        } else {
+            res.status(400).json({ error: 'USER NOT FOUND' })
+        }
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'ERROR GET DATA' })
+    }
+}
+
+export default { getsUserList, getUserList }
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,24 +1,18 @@
-// import user from '../services/user.json' assert { type: "json"}
-
-import getUserList from '../controllers/user.js'
+import { getsUserList, getUserList } from '../controllers/user.js'
 
 import Router from 'express'
 
 export const router = Router()
 
 
-router.get('/user', getUserList)
+router.get('/user', getsUserList)
 
-router.post('/user', (req, res) => {
+router.get('/user/:id', getUserList)
 
-})
+// router.post('/user', postUserList)
 
-router.put('/user', (req, res) => {
+// router.patch('/user/:id', patUserList)
 
-})
-
-router.delete('/user', (req, res) => {
-
-})
+// router.delete('/user/:id', delUserList)
 
 export default router
